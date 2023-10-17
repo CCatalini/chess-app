b805fc8afb940f4f3ff15598316eb2e2d7f54c00
ccatalini <camila.catalini@ing.austral.edu.ar>
2023-10-17 15:37:22 -0300

    AndValidator y OrValidator implemented with test

---
+++ app/src/test/kotlin/ValidatorTest.kt
@@ -0,0 +1,64 @@
+import engine.GameStateImpl
+import engine.board.BoardImpl
+import engine.board.Position
+import engine.movement.Movement
+import engine.movement.validator.*
+import engine.piece.Color
+import engine.piece.Pawn
+import org.junit.jupiter.api.Test
+
+internal class ValidatorTest {
+
+    //test de santi para que vea como funciona el andValidator para los validadores
+    @Test
+    fun testAndValidator() {
+        val validator : Validator = AndValidator(
+            listOf(
+                StraightValidator(1, 1),
+                LegalPositionValidator()))
+
+        val gameState: GameState = GameStateImpl(
+            listOf(
+                BoardImpl(8,8, mapOf()) ))
+
+        val movement : Movement = Movement( Position(0,0), Position(9,0) )
+
+        println(validator.validate(movement, gameState))
+    }
+
+    @Test
+    fun testOrValidator() {
+        val validator : Validator = OrValidator(
+            listOf(
+                StraightValidator(1, 1),
+                LegalPositionValidator()))
+
+        val gameState: GameState = GameStateImpl(
+            listOf(
+                BoardImpl(8,8, mapOf()) ))
+
+        val movement : Movement = Movement( Position(0,0), Position(9,0) )
+
+        println(validator.validate(movement, gameState))
+    }
+
+
+
+
+    @Test
+    fun cositas(){
+        val gameState: GameState = GameStateImpl(
+            listOf(
+                BoardImpl(8, 8, mapOf(
+                    Position(0, 0) to Pawn("1", Color.BLACK, listOf())
+                ))
+            )
+        )
+
+        println(gameState.getCurrentBoard().getPieceByPosition(Position(0,0))!!.getId())
+
+    }
+
+
+
+}
