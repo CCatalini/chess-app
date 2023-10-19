ea897a7aa39ed7d7c5b92bc469adea9fe3a8941a
ccatalini <camila.catalini@ing.austral.edu.ar>
2023-10-19 16:18:17 -0300

    StraightEmptyPathValidator implemented

---
+++ app/src/main/kotlin/engine/validator/obstacle/DiagonalEmptyPathValidator.kt
@@ -0,0 +1,12 @@
+package engine.validator.obstacle
+
+import GameState
+import engine.movement.Movement
+import engine.validator.Validator
+
+class DiagonalEmptyPathValidator : Validator {
+
+    override fun validate(movement: Movement, gameState: GameState): Boolean {
+        TODO("Not yet implemented")
+    }
+}
