9abc89e93acbfb7d4f85a7c88a370fd311af09ce
ccatalini <camila.catalini@ing.austral.edu.ar>
2023-11-22 18:08:03 -0300

    diagonal empty path testeado y fixeado

--- app/src/main/kotlin/edu/austral/dissis/common/validator/obstacle/DiagonalEmptyPathValidator.kt
+++ app/src/main/kotlin/edu/austral/dissis/common/validator/obstacle/DiagonalEmptyPathValidator.kt
@@ -1,32 +1,39 @@
 package edu.austral.dissis.common.validator.obstacle
 
 import edu.austral.dissis.chess.game.IGameState
-import edu.austral.dissis.common.board.Board
 import edu.austral.dissis.common.board.Position
 import edu.austral.dissis.chess.movement.Movement
+import edu.austral.dissis.common.validator.Validator
+import edu.austral.dissis.common.validator.ValidatorResponse
+import kotlin.math.abs
 
-class DiagonalEmptyPathValidator : edu.austral.dissis.common.validator.Validator {
+class DiagonalEmptyPathValidator : Validator {
 
-    override fun validate(movement: Movement, gameState: IGameState): edu.austral.dissis.common.validator.ValidatorResponse {
-        val positions: Board = gameState.getCurrentBoard() as Board
-
-        val fromX = movement.from.row
-        val fromY = movement.from.column
-        val toX = movement.to.row
-        val toY = movement.to.column
-        var currentX = fromX + 1
-        var currentY = fromY + 1
-
-        while (currentX < toX && currentY < toY) {
-            val positionToCheck = Position(currentX, currentY)
-            if (positions.getPieceByPosition(positionToCheck) != null) {
-                return edu.austral.dissis.common.validator.ValidatorResponse.ValidatorResultInvalid("Hay piezas en el camino")
-            }
-            currentX++
-            currentY++
+    override fun validate(movement: Movement, gameState: IGameState): ValidatorResponse {
+        if(abs(movement.from.row - movement.to.row) != abs(movement.from.column - movement.to.column)){
+            return ValidatorResponse.ValidatorResultInvalid("No es un movimiento diagonal")
         }
 
-        return edu.austral.dissis.common.validator.ValidatorResponse.ValidatorResultValid("OK")
+        var currentCoordinate = movement.from
+
+        while (currentCoordinate.row != movement.to.row && currentCoordinate.column != movement.to.column){
+            currentCoordinate =
+                if (currentCoordinate.row < movement.to.row && currentCoordinate.column < movement.to.column){
+                    Position(currentCoordinate.row + 1, currentCoordinate.column + 1)
+                }else if (currentCoordinate.row < movement.to.row){
+                    Position(currentCoordinate.row + 1, currentCoordinate.column - 1)
+                }else if (currentCoordinate.column < movement.to.column){
+                    Position(currentCoordinate.row - 1, currentCoordinate.column + 1)
+                }else {
+                    Position(currentCoordinate.row - 1, currentCoordinate.column - 1)
+                }
+            if (currentCoordinate.row != movement.to.row && currentCoordinate.column != movement.to.column && gameState.getCurrentBoard().getPieceByPosition(currentCoordinate) != null){
+                return ValidatorResponse.ValidatorResultInvalid("Hay piezas en el camino")
+            }
+        }
+
+        return ValidatorResponse.ValidatorResultValid("OK")
     }
 
 }
+
---
+++ app/src/test/kotlin/chess/validator/EmptyPathsTest.kt
@@ -0,0 +1,63 @@
+package chess.validator
+
+import edu.austral.dissis.chess.factory.BoardFactory
+import edu.austral.dissis.chess.factory.pieceInit.KnightInitializer
+import edu.austral.dissis.chess.factory.pieceInit.RookInitializer
+import edu.austral.dissis.chess.game.GameState
+import edu.austral.dissis.chess.movement.Movement
+import edu.austral.dissis.chess.piece.Color
+import edu.austral.dissis.chess.piece.Piece
+import edu.austral.dissis.chess.validator.turn.ChessTurnValidator
+import edu.austral.dissis.chess.validator.winCondition.CheckMateValidator
+import edu.austral.dissis.common.board.Board
+import edu.austral.dissis.common.board.Position
+import edu.austral.dissis.common.validator.ValidatorResponse
+import edu.austral.dissis.common.validator.obstacle.DiagonalEmptyPathValidator
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EmptyPathsTest {
+
+    @Test
+    fun testDiagonalEmptyPathValid() {
+        val gameState = GameState(listOf( Board(8,8, emptyMap())),
+            CheckMateValidator(),
+            ChessTurnValidator(Color.WHITE),
+            listOf(),
+            listOf())
+        val validator = DiagonalEmptyPathValidator()
+
+        val from = Position(2, 2)
+        val to = Position(5, 5)
+        val movement = Movement(from, to)
+
+        val result = validator.validate(movement, gameState)
+
+        assertEquals(ValidatorResponse.ValidatorResultValid("OK"), result)
+    }
+
+    @Test
+    fun testDiagonalNotEmptyPathInvalid() {
+
+        val map: Map<Position, Piece> = mutableMapOf(
+            Position(2, 2) to RookInitializer().initialize(Color.WHITE),
+            Position(3,3) to KnightInitializer().initialize(Color.WHITE)
+        )
+
+        val gameState = GameState(listOf( Board(8, 8, map)),
+            CheckMateValidator(),
+            ChessTurnValidator(Color.WHITE),
+            listOf(),
+            listOf())
+        val validator = DiagonalEmptyPathValidator()
+
+        val from = Position(2, 2)
+        val to = Position(5, 5)
+        val movement = Movement(from, to)
+
+        val result = validator.validate(movement, gameState)
+
+        assertEquals(ValidatorResponse.ValidatorResultInvalid("Hay piezas en el camino"), result)
+    }
+
+}
